b1c2b5b2f158dd4a8de4fa96fdc0f0e4d25c5688
Siddhanttade <siddhanttade19@gmail.com>
2024-07-30 13:23:54 +0000

    Conversion of Datatypes

---
+++ 1_Basics/3_ConversionOperation.js
@@ -0,0 +1,25 @@
+let score="33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueinNumber=Number(score);
+console.log(typeof valueinNumber);
+
+//"33"=>string
+//"33abc"=>NaN
+//true=>1;false=>
+
+let isLoggedIn=1
+
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//1=>true;0=>False
+//"sid"=>true;""=>false
+
+let number=45
+
+let stringNumber=String(number);
+console.log(stringNumber)
+console.log(typeof stringNumber)
