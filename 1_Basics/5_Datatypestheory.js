dec7db9f4dde674725029ec49cc0c247005d886f
Siddhanttade <siddhanttade19@gmail.com>
2024-07-31 12:38:39 +0000

    datatypes summary

---
+++ 1_Basics/5_Datatypestheory.js
@@ -0,0 +1,26 @@
+//primtive=>the datatypes which are call by value meaning whenever they are called original data reference is not given from memory rather a copy is given and the changes you make are saved in the copy
+
+//String,bigint,number,boolean,null,undefined,symbol
+
+const score=33
+const bigNum=328568435435854359539n
+const num=45
+const isLoggedIn=true
+let outsideTemp=null
+
+let userName;
+
+const id=Symbol('123')
+const anotherId=Symbol('123') //Both are different
+console.log(id===anotherId)
+
+//Reference(non Primitive)
+//array,obect,function
+
+const heroes=["sid","sum","kun"]
+let obj={
+    name:"siddhant",
+    class:TY,
+}
+
+//REMEMBER=>if we try to get the value of null by using typeof method then it shows object, similarly nonprimitive datatype also show object as its datatype. If we see typeof(function) it is function object
