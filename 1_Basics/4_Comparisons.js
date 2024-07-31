46ef200d923619f7fd3f877a9fc275b7ef4f1b00
Siddhanttade <siddhanttade19@gmail.com>
2024-07-31 12:19:16 +0000

    comparing different datatypes

---
+++ 1_Basics/4_Comparisons.js
@@ -0,0 +1,9 @@
+//while comparing 2 diff entities it is necessary to keep their datatype same
+
+console.log(null>0);
+console.log(null==0);
+console.log (null>=0);
+
+console.log(undefined>0);
+
+//when strict checking is done using === it automatically converts the dattatype and checks whether the comparison is possible or not 
